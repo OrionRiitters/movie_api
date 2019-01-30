26f54f9bf194014b6d0ca5ed3579ab9aa7ca76ae
Orion Riitters <orionriitters@gmail.com>
2019-01-29 23:05:41 -0600

    Added a README.md describing the application.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Movie API
+This application was created to pull information from Rotten Tomatoes via Beautiful Soup and the Open Movie Database API in order to display it on a simple JS GUI. Python is used on the back end as a server to handle HTTP requests from the GUI. Python is also used to scrape Rotten Tomatoes and access the OMDB API. The resulting information is fed back to the JS front end. 
---
+++ web_server/serv.py
@@ -0,0 +1,30 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import time
+
+class Serv(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path =='/':
+            self.path = '/index.html'
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+
+
+
+try:
+	server = HTTPServer(('', 8080), Serv)
+	print('Started httpserver on port ' , 8080)
+	server.serve_forever()
+
+except KeyboardInterrupt:
+	print('^C received, shutting down the web server')
+	server.socket.close()
+	
+
